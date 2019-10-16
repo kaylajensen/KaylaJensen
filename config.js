139c553d1cc61a830053ac30503c83b15460f939
Kayla Jensen <kaylanicolejensen@gmail.com>
2019-10-16 06:52:50 -0400

    Add theme

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'Kayla Jensen',
+  manifestName: 'Kayla Jensen',
+  manifestShortName: 'Kayla Jensen',
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#CEA2AC',
+  manifestThemeColor: '#CEA2AC',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/k-icon.png',
+  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Kayla Jensen',
+  subHeading: 'Engineer. Crafter. Baker.',
+  // social
+  socialLinks: [
+    {
+      style: 'brands',
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/kaylajensen',
+    },
+    {
+      style: 'brands',
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://instagram.com/',
+    },
+    {
+      style: 'solid',
+      icon: 'fa-envelope',
+      name: 'Email',
+      url: 'mailto:kaylanicolejensen@gmail.com',
+    },
+  ],
+};
